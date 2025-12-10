d2be579ab90a028560e7fd7bb4df990c4be50355
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-12-10 09:27:28 +0530

    fix: update healthcheck response format

--- src/api/v1/healthcheck/healthcheck.controllers.js
+++ src/api/v1/healthcheck/healthcheck.controllers.js
@@ -3,5 +3,5 @@ import { APIResponse } from '../../response.api.js';
 
 // @controller GET /
 export const healthCheck = (_, res) => {
-  return res.status(200).json(new APIResponse(200, 'Server is Running'));
+  return res.status(200).json(new APIResponse(200, { message: 'Server is Running' }));
 };
