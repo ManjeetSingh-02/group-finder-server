b05ba958d122fe47f503cc29315da860b1d312a1
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-12-24 11:44:20 +0530

    fix: update error message keys in user role and profile schemas

--- src/api/v1/user/user.zodschemas.js
+++ src/api/v1/user/user.zodschemas.js
@@ -7,7 +7,7 @@ import { z } from 'zod';
 // zod schema for update user role
 export const updateUserRoleSchema = z.object({
   body: z.object({
-    userEmail: z.email({ message: 'userEmail is required' }).trim().toLowerCase(),
+    userEmail: z.email({ error: 'userEmail is required' }).trim().toLowerCase(),
     newRole: z.enum(availableUserRoles),
   }),
 });
@@ -18,25 +18,25 @@ export const updateUserProfileSchema = z.object({
     newUserExpertise: z
       .array(
         z.object({
-          roleName: z.string().min(1, { message: 'roleName is required' }).trim(),
+          roleName: z.string().min(1, { error: 'roleName is required' }).trim(),
           techStack: z
             .array(
               z.object({
-                skillName: z.string().min(1, { message: 'skillName is required' }).trim(),
+                skillName: z.string().min(1, { error: 'skillName is required' }).trim(),
                 experienceInMonths: z
                   .number()
-                  .min(0, { message: 'experienceInMonths must be at least 0' }),
+                  .min(1, { error: 'experienceInMonths must be at least 1' }),
               })
             )
-            .min(1, { message: 'At least one techStack is required' }),
+            .min(1, { error: 'At least one techStack is required' }),
         })
       )
       .optional(),
     newSocialLinks: z
       .array(
         z.object({
-          platformName: z.string().min(1, { message: 'platformName is required' }).trim(),
-          platformURL: z.url({ message: 'Valid platformURL is required' }).trim(),
+          platformName: z.string().min(1, { error: 'platformName is required' }).trim(),
+          platformURL: z.url({ error: 'Valid platformURL is required' }).trim(),
         })
       )
       .optional(),
