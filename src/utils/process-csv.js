ff0436efeabe983910b7d5da1ca205614f15e5d2
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-12-27 22:07:27 +0530

    refactor: remove unused CSV parsing functionality from process-csv utility

--- src/utils/process-csv.js
+++ src/utils/process-csv.js
@@ -3,8 +3,6 @@ import { CSV_UPLOAD_CONFIG } from './constants.js';
 
 // import external modules
 import multer from 'multer';
-import { Readable } from 'stream';
-import csvParser from 'csv-parser';
 
 // sub-function to validate file type
 function filterCSVType(_, file, cb) {
@@ -23,23 +21,3 @@ export const uploadCSVFiles = multer({
     maxCount: CSV_UPLOAD_CONFIG.MAX_FILE_COUNT,
   },
 ]);
-
-// function to parse one CSV file buffer
-export async function parseCSVFile(fileBuffer) {
-  // array to hold parsed data
-  const parsedDataArray = [];
-
-  // return a promise that resolves with parsed data or rejects with an error
-  return new Promise((resolve, reject) => {
-    Readable.from(fileBuffer)
-      .pipe(
-        csvParser({
-          mapHeaders: ({ header }) => header.trim().toLowerCase(),
-          mapValues: ({ value }) => value.trim(),
-        })
-      )
-      .on('data', parseData => parsedDataArray.push(parseData))
-      .on('error', parseError => reject(parseError))
-      .on('end', () => resolve(parsedDataArray));
-  });
-}
