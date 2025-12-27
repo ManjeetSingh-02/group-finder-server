25985ed806a32e07ad7ec066c783fd4d42f55819
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-12-27 22:02:31 +0530

    refactor: implement Google OAuth2 login and registration flow with cohort validation in controllers file

--- src/api/v1/auth/auth.controllers.js
+++ src/api/v1/auth/auth.controllers.js
@@ -6,10 +6,9 @@ import {
   REFRESH_TOKEN_COOKIE_CONFIG,
 } from '../../../utils/constants.js';
 import { asyncHandler } from '../../../utils/async-handler.js';
-import { handleGoogleLogin } from '../../../utils/google-auth.js';
 import { APIError } from '../../error.api.js';
 import { APIResponse } from '../../response.api.js';
-import { User } from '../../../models/index.js';
+import { Cohort, User } from '../../../models/index.js';
 
 // import external modules
 import crypto from 'crypto';
@@ -146,3 +145,82 @@ export const googleLogout = asyncHandler(async (req, res) => {
     .clearCookie(REFRESH_TOKEN_COOKIE_CONFIG.NAME, REFRESH_TOKEN_COOKIE_CONFIG.OPTIONS)
     .json(new APIResponse(200, { message: 'Logout Successful' }));
 });
+
+// sub-function to handle login and registration flow for Google OAuth2
+async function handleGoogleLogin(userDetails) {
+  // check if user is already registered
+  const existingUser = await User.findOne({ email: userDetails.email }).select(
+    'googleID refreshToken'
+  );
+
+  // --------------------------------------------------
+  // LOGIN PATH: User is already registered
+  // --------------------------------------------------
+
+  // If user exists, check for any missing googleID (Mainly SYSTEM_ADMIN's) and update it
+  if (existingUser) {
+    if (!existingUser.googleID) {
+      // update googleID in db
+      existingUser.googleID = userDetails.googleID;
+
+      // save user
+      await existingUser.save();
+    }
+
+    return generateTokens(existingUser);
+  }
+
+  // --------------------------------------------------
+  // REGISTRATION PATH: User is not registered
+  // --------------------------------------------------
+
+  // Check if user is allowed in any cohorts
+  const allowedCohorts = await Cohort.find({
+    allowedUserEmails: { $in: [userDetails.email] },
+  })
+    .select('_id')
+    .lean();
+
+  // if not allowed, throw error
+  if (allowedCohorts.length === 0)
+    throw new APIError(403, {
+      type: 'Google Login Callback Error',
+      message: 'You are not authorized to access this application.',
+    });
+
+  // create a new user in db
+  const newUser = await User.create({
+    googleID: userDetails.googleID,
+    email: userDetails.email,
+    username: `${userDetails.email.split('@')[0].slice(0, 10)}${generateRandomSuffix()}`,
+  });
+  if (!newUser)
+    throw new APIError(500, {
+      type: 'Google Login Callback Error',
+      message: 'Something went wrong while creating a new user.',
+    });
+
+  return generateTokens(newUser);
+}
+
+// sub-function to generate access and refresh tokens
+async function generateTokens(user) {
+  const accessToken = user.generateAccessToken();
+  const refreshToken = user.generateRefreshToken();
+
+  // update refresh token in db
+  user.refreshToken = refreshToken;
+
+  // save user
+  await user.save({ validateBeforeSave: false });
+
+  // return access and refresh tokens
+  return { accessToken, refreshToken };
+}
+
+// sub-function to generate a random suffix for username
+function generateRandomSuffix() {
+  return Math.floor(Math.random() * 65536)
+    .toString(16)
+    .padStart(4, '0');
+}
