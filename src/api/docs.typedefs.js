2c2813f9ec2ef88c206b80dc44fd1f10c3da3ddf
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-01-02 18:24:59 +0530

    feat: refine API response typedefs with required properties and clearer descriptions

--- src/api/docs.typedefs.js
+++ src/api/docs.typedefs.js
@@ -1,31 +1,31 @@
 /**
  * Standard structure for success details in API responses.
  * @typedef {object} SuccessData
- * @property {string} message - Success message (Always present)
- * @property {object} [data] - The data returned (Optional payload)
+ * @property {string} message.required - Descriptive success message
+ * @property {object | Array<object>} data - Actual data returned from the API
  */
 
 /**
- * Standard structure for successful API responses.
+ * Standard structure for success API responses.
  * @typedef {object} APISuccessResponse
- * @property {boolean} success - Indicates the request was successful
- * @property {number} statusCode - The HTTP status code
- * @property {SuccessData} response - The container for the message and data
+ * @property {boolean} success.required - Always true for success responses
+ * @property {number} statusCode.required - HTTP status code
+ * @property {SuccessData} response.required - Container for success response details
  */
 
 /**
  * Standard structure for error details in API responses.
  * @typedef {object} ErrorData
- * @property {string} type - The category of error
- * @property {string} message - A human-readable description
- * @property {string[]} [issues] - List of specific issues (Optional)
+ * @property {string} type.required - Category of error
+ * @property {string} message.required - Descriptive error message
+ * @property {Array<string>} issues - List of specific issues encountered
  */
 
 /**
  * Standard structure for all error API responses.
  * @typedef {object} APIErrorResponse
- * @property {boolean} success - Always false for error responses
- * @property {number} statusCode - The HTTP status code
- * @property {ErrorData} error - The container for error details
+ * @property {boolean} success.required - Always false for error responses
+ * @property {number} statusCode.required - HTTP status code
+ * @property {ErrorData} error.required - Container for error response details
  *
  */
